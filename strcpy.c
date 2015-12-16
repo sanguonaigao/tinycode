309fc5dfac16ed3ac5791f9502aa3bb68a6f146c
zpengwei@yeah.net <zpengwei@yeah.net>
2015-12-16 13:16:38 +0800

    delete this codes!!!
    Signed-off-by: zpengwei@yeah.net <zpengwei@yeah.net>

--- strcpy.c
+++
@@ -1,9 +0,0 @@
-char * __cdecl strcpy(char * dst, const char * src)
-{
-        char * cp = dst;
-
-        while( *cp++ = *src++ )
-                ;               /* Copy src over dst */
-
-        return( dst );
-}
